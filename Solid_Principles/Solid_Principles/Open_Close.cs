4b2d39967fde695f48ecb5e8a4cffc211995638e
Smita <148760934+Smita-catchpoint@users.noreply.github.com>
2024-01-09 17:42:49 +0530

    Add files via upload

---
+++ Solid_Principles/Solid_Principles/Open_Close.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Principles
+{
+    public interface Shape
+    {
+        double Area();
+    }
+
+    public class Rectangle : Shape
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        public double Area()
+        {
+            return Length * Width;
+        }
+    }
+
+
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+
+    public class AreaCalculator
+    {
+        public double TotalArea(List<Shape> shapes)
+        {
+            double area = 0;
+            foreach (var shape in shapes)
+            {
+                area += shape.Area();
+            }
+            return area;
+        }
+    }
+
+
+    public class Open_Close
+    {
+        public static void Main(string[] args)
+        {
+
+            List<Shape> shapes = new List<Shape>();
+
+            shapes.Add(new Rectangle { Length = 10, Width = 5 });
+            shapes.Add(new Circle { Radius = 3 });
+
+
+            AreaCalculator calculator = new AreaCalculator();
+            double totalArea = calculator.TotalArea(shapes);
+            Console.WriteLine($"The total area is {totalArea}");
+
+            foreach (var s in shapes)
+            {
+
+                string shapeName = s.GetType().Name;
+                double shapeArea = s.Area();
+
+                Console.WriteLine($"{shapeName} area is {shapeArea}");
+
+            }
+        }
+    }
+}
---
+++ Solid_Principles/Solid_Principles/SRP.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Principles
+{
+    internal class SRP
+    {
+    }
+
+    // represents the data of an invoice
+    // this class only has one reason to change (the data of an invoice)
+    public class Invoice
+    {
+        public int InvoiceNo { get; set; }
+        public DateOnly IssuedDate { get; set; }
+        public string? Customer { get; set; }
+        public decimal Amount { get; set; }
+        public string? Status { get; set; }
+    }
+
+    //  responsible for printing an invoice
+    //  this class only has one reason to change (the logic of printing an invoice)
+    public class InvoicePrinter
+    {
+        public void Print(Invoice invoice)
+        {
+           
+            Console.WriteLine($"Invoice No: {invoice.InvoiceNo}");
+            Console.WriteLine($"Issued Date: {invoice.IssuedDate}");
+            Console.WriteLine($"Customer: {invoice.Customer}");
+            Console.WriteLine($"Amount: {invoice.Amount}");
+            Console.WriteLine($"Status: {invoice.Status}");
+        }
+    }
+
+    // responsible for storing and retrieving invoices
+    // reason to change (the logic of data access)
+    public class InvoiceRepository
+    {
+        // A list to store invoices in memory
+         List<Invoice> invoices = new List<Invoice>();
+
+      
+        public void Save(Invoice invoice)
+        {
+          
+            invoices.Add(invoice);
+        }
+
+      
+        public Invoice GetByInvoiceNo(int invoiceNo)
+        {
+          
+            return invoices.Find(i => i.InvoiceNo == invoiceNo);
+        }
+    }
+
+ 
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // invoice object
+            Invoice invoice = new Invoice
+            {
+                InvoiceNo = 1,
+                IssuedDate = DateOnly.FromDateTime(DateTime.Now),
+                Customer = "John Doe",
+                Amount = 100,
+                Status = "Paid"
+            };
+           
+
+
+            InvoiceRepository repository = new InvoiceRepository();
+            repository.Save(invoice);
+
+            Invoice invoiceFromDb = repository.GetByInvoiceNo(1);
+
+        
+            InvoicePrinter printer = new InvoicePrinter();
+            printer.Print(invoiceFromDb);
+        }
+    }
+
+}
