454307f4ff0d149f3329af9ad20978d48ad616d9
Smita <148760934+Smita-catchpoint@users.noreply.github.com>
2024-01-09 17:42:01 +0530

    Add files via upload

---
+++ ParallelProgramming/GitDemo/GitDemo/FileName.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemo
+{
+    internal class FileName
+    {
+        static void Main()
+        {
+            int[] numbers = Enumerable.Range(1, 1000000).ToArray();
+            // Regular LINQ Query
+            Stopwatch regularQueryTimer = Stopwatch.StartNew();
+            var squares = numbers.Select(num => num * num).ToArray();
+            regularQueryTimer.Stop();
+            Console.WriteLine($"Regular LINQ took: {regularQueryTimer.ElapsedMilliseconds} ms");
+
+            // Parallel LINQ Query
+            Stopwatch parallelQueryTimer = Stopwatch.StartNew();
+            var squaresParallel = numbers.AsParallel().Select(num => num * num).ToArray();
+            parallelQueryTimer.Stop();
+            Console.WriteLine($"Parallel LINQ took: {parallelQueryTimer.ElapsedMilliseconds} ms");
+        }
+    }
+}
---
+++ ParallelProgramming/ParallelProgram/ParallelProgram/MultithreadingDemo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelProgrammingDemo
+{
+    class MultithreadingDemo
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread((Method1) => { Console.WriteLine("Hello"); });
+            t1.Start();
+            Thread t2 = new Thread((Method2) => {  Console.WriteLine("World"); });
+            t2.Start();
+        }
+
+        
+
+    }
+}
